d25ec37ef1f98f26e1e05120633994d3b48effe1
Nate <89227631+natedotv2@users.noreply.github.com>
2023-08-01 11:11:06 +0100

    Added UnityCore reward tokens

---
+++ contracts/Tokens/URT/URTConverter.sol
@@ -0,0 +1,161 @@
+pragma solidity ^0.5.16;
+
+import "../../Utils/IERC20.sol";
+import "../../Utils/SafeERC20.sol";
+import "../UCore/IUCoreVesting.sol";
+import "./URTConverterStorage.sol";
+import "./URTConverterProxy.sol";
+
+/**
+ * @title Venus's URTConversion Contract
+ * @author Venus
+ */
+contract URTConverter is URTConverterStorage {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    address public constant DEAD_ADDRESS = 0x000000000000000000000000000000000000dEaD;
+
+    /// @notice decimal precision for URT
+    uint256 public constant urtDecimalsMultiplier = 1e18;
+
+    /// @notice decimal precision for UCore
+    uint256 public constant ucoreDecimalsMultiplier = 1e18;
+
+    /// @notice Emitted when an admin set conversion info
+    event ConversionInfoSet(
+        uint256 conversionRatio,
+        uint256 conversionStartTime,
+        uint256 conversionPeriod,
+        uint256 conversionEndTime
+    );
+
+    /// @notice Emitted when token conversion is done
+    event TokenConverted(
+        address reedeemer,
+        address urtAddress,
+        uint256 urtAmount,
+        address ucoreAddress,
+        uint256 ucoreAmount
+    );
+
+    /// @notice Emitted when an admin withdraw converted token
+    event TokenWithdraw(address token, address to, uint256 amount);
+
+    /// @notice Emitted when UCoreVestingAddress is set
+    event UCoreVestingSet(address ucoreVestingAddress);
+
+    constructor() public {}
+
+    function initialize(
+        address _urtAddress,
+        address _ucoreAddress,
+        uint256 _conversionRatio,
+        uint256 _conversionStartTime,
+        uint256 _conversionPeriod
+    ) public {
+        require(msg.sender == admin, "only admin may initialize the URTConverter");
+        require(initialized == false, "URTConverter is already initialized");
+
+        require(_urtAddress != address(0), "urtAddress cannot be Zero");
+        urt = IERC20(_urtAddress);
+
+        require(_ucoreAddress != address(0), "ucoreAddress cannot be Zero");
+        ucore = IERC20(_ucoreAddress);
+
+        require(_conversionRatio > 0, "conversionRatio cannot be Zero");
+        conversionRatio = _conversionRatio;
+
+        require(_conversionStartTime >= block.timestamp, "conversionStartTime must be time in the future");
+        require(_conversionPeriod > 0, "_conversionPeriod is invalid");
+
+        conversionStartTime = _conversionStartTime;
+        conversionPeriod = _conversionPeriod;
+        conversionEndTime = conversionStartTime.add(conversionPeriod);
+        emit ConversionInfoSet(conversionRatio, conversionStartTime, conversionPeriod, conversionEndTime);
+
+        totalUrtConverted = 0;
+        _notEntered = true;
+        initialized = true;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     */
+    modifier nonReentrant() {
+        require(_notEntered, "re-entered");
+        _notEntered = false;
+        _;
+        _notEntered = true; // get a gas-refund post-Istanbul
+    }
+
+    /**
+     * @notice sets UCoreVestingProxy Address
+     * @dev Note: If UCoreVestingProxy is not set, then Conversion is not allowed
+     * @param _ucoreVestingAddress The UCoreVestingProxy Address
+     */
+    function setUCoreVesting(address _ucoreVestingAddress) public {
+        require(msg.sender == admin, "only admin may initialize the Vault");
+        require(_ucoreVestingAddress != address(0), "ucoreVestingAddress cannot be Zero");
+        ucoreVesting = IUCoreVesting(_ucoreVestingAddress);
+        emit UCoreVestingSet(_ucoreVestingAddress);
+    }
+
+    modifier isInitialized() {
+        require(initialized == true, "URTConverter is not initialized");
+        _;
+    }
+
+    function isConversionActive() public view returns (bool) {
+        uint256 currentTime = block.timestamp;
+        if (currentTime >= conversionStartTime && currentTime <= conversionEndTime) {
+            return true;
+        }
+        return false;
+    }
+
+    modifier checkForActiveConversionPeriod() {
+        uint256 currentTime = block.timestamp;
+        require(currentTime >= conversionStartTime, "Conversion did not start yet");
+        require(currentTime <= conversionEndTime, "Conversion Period Ended");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin can");
+        _;
+    }
+
+    modifier nonZeroAddress(address _address) {
+        require(_address != address(0), "Address cannot be Zero");
+        _;
+    }
+
+    /**
+     * @notice Transfer URT and redeem UCore
+     * @dev Note: If there is not enough UCore, we do not perform the conversion.
+     * @param urtAmount The amount of URT
+     */
+    function convert(uint256 urtAmount) external isInitialized checkForActiveConversionPeriod nonReentrant {
+        require(
+            address(ucoreVesting) != address(0) && address(ucoreVesting) != DEAD_ADDRESS,
+            "UCore-Vesting Address is not set"
+        );
+        require(urtAmount > 0, "URT amount must be non-zero");
+        totalUrtConverted = totalUrtConverted.add(urtAmount);
+
+        uint256 redeemAmount = urtAmount.mul(conversionRatio).mul(ucoreDecimalsMultiplier).div(1e18).div(
+            urtDecimalsMultiplier
+        );
+
+        emit TokenConverted(msg.sender, address(urt), urtAmount, address(ucore), redeemAmount);
+        urt.safeTransferFrom(msg.sender, DEAD_ADDRESS, urtAmount);
+        ucoreVesting.deposit(msg.sender, redeemAmount);
+    }
+
+    /*** Admin Functions ***/
+    function _become(URTConverterProxy urtConverterProxy) public {
+        require(msg.sender == urtConverterProxy.admin(), "only proxy admin can change brains");
+        urtConverterProxy._acceptImplementation();
+    }
+}
