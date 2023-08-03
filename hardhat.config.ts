7188be0a8894daf72492c196b4d7202e86f09abc
Nate <89227631+natedotv2@users.noreply.github.com>
2023-08-03 22:30:17 +0100

    updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
@@ -34,6 +36,37 @@ const config: HardhatUserConfig = {
         },
       },
     ],
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: false,
+  },
+  networks: {
+    mainnet: {
+      url: `https://rpc.coredao.org`,
+      chainId: 1116,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    testnet: {
+      url: `https://rpc.test.btcs.network`,
+      chainId: 1115,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/9272c529b110464abab493da913448a2`,
+      chainId: 4,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    coverage: {
+      url: "http://localhost:8555",
+    },
+
+    localhost: {
+      url: `http://127.0.0.1:8545`,
+    },
+  },
+  etherscan: {
+    apiKey: "c808613489e847cea9dbc7cca641afab",
   },
 };
 
